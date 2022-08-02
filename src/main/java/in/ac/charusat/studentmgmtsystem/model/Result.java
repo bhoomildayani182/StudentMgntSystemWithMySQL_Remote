e81cc3a3ef56097dad2dcf1459dd8bd33b1399ef
Bhoomil Dayani <bhoomildayani182@gmail.com>
2022-08-02 13:07:49 +0530

    MySQLRemote

---
+++ src/main/java/in/ac/charusat/studentmgmtsystem/model/Result.java
@@ -0,0 +1,39 @@
+package in.ac.charusat.studentmgmtsystem.model;
+
+import javax.persistence.Entity;
+import javax.persistence.Id;
+@Entity
+public class Result {
+
+    @Id
+    private Integer id;
+    private String semester;
+    private Double percentage;
+
+    public Result() {
+    }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getSemester() {
+        return semester;
+    }
+
+    public void setSemester(String semester) {
+        this.semester = semester;
+    }
+
+    public Double getPercentage() {
+        return percentage;
+    }
+
+    public void setPercentage(Double percentage) {
+        this.percentage = percentage;
+    }
+}
