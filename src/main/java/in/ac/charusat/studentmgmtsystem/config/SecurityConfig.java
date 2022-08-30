d721d55239ed7da4c436d03cd88384e97446096c
Bhoomil Dayani <bhoomildayani182@gmail.com>
2022-08-30 21:54:00 +0530

    Added security with custom users in memory authentication And Added security with custom users jdbc authentication using H2 database.

--- src/main/java/in/ac/charusat/studentmgmtsystem/config/SecurityConfig.java
+++ src/main/java/in/ac/charusat/studentmgmtsystem/config/SecurityConfig.java
@@ -1,26 +1,54 @@
 package in.ac.charusat.studentmgmtsystem.config;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.core.userdetails.User;
 import org.springframework.security.crypto.password.NoOpPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
+
+import javax.sql.DataSource;
 
 @EnableWebSecurity
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
 //    Go to right click and go to genrator to override methon and
+
+    @Autowired
+    DataSource dataSource;
+
     @Override
     protected void configure(AuthenticationManagerBuilder auth) throws Exception {
-        auth.inMemoryAuthentication()
-                .withUser("admin")
-                .password("admin")
-                .roles("ADMIN")
-                .and()
-                .withUser("20IT022")
-                .password("20IT022")
-                .roles("USER");
+        auth.jdbcAuthentication().dataSource(dataSource)
+                .withDefaultSchema()
+                .withUser(
+                        User.withUsername("admin")
+                                .password("admin")
+                                .roles("ADMIN")
+                )
+                .withUser(
+                        User.withUsername("user")
+                                .password("user")
+                                .roles("USER")
+                );
+    //        auth.inMemoryAuthentication()
+//                .withUser("admin")
+//                .password("admin")
+//                .roles("ADMIN")
+//                .and()
+//                .withUser("20IT022")
+//                .password("20IT022")
+//                .roles("USER");
     }
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http.csrf().disable();
+        http.headers().frameOptions().disable();
+    }
+
     @Bean
 //    here password is ancoding so this mathod to create any method ancoding and decoding
     public PasswordEncoder getpasswordEncoder(){
