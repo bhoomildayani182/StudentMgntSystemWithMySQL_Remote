cac9a922a7aaba18c2469b5522f48cdf10616db4
Bhoomil Dayani <bhoomildayani182@gmail.com>
2022-11-08 18:19:50 +0530

    Add login functonality

--- src/main/java/in/ac/charusat/studentmgmtsystem/controller/UserController.java
+++ src/main/java/in/ac/charusat/studentmgmtsystem/controller/UserController.java
@@ -1,42 +1,65 @@
 package in.ac.charusat.studentmgmtsystem.controller;
 
+import in.ac.charusat.studentmgmtsystem.model.LoginRequest;
+import in.ac.charusat.studentmgmtsystem.model.Student;
 import in.ac.charusat.studentmgmtsystem.model.User;
 import in.ac.charusat.studentmgmtsystem.repository.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.mail.SimpleMailMessage;
+import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
-import org.springframework.mail.SimpleMailMessage;
-import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.security.core.context.SecurityContextHolder;
 
 import java.util.List;
 
 @RestController
-//@CrossOrigin(origins = "http://localhost:3000")
+@CrossOrigin(origins = "http://localhost:3000")
 public class UserController {
     @Autowired
     private JavaMailSender javaMailSender;
 
-
+    @Autowired
+    AuthenticationManager authenticationManager;
     @Autowired
     UserRepository userRepository;
 
+    @GetMapping("/user")
+    public List<User> getAllStudents() {
+        return userRepository.findAll();
+    }
     @PostMapping("/user")
     public List<User> addUser(@RequestBody User user) {
         userRepository.save(user);
-        sendEmail();
+        sendEmail(user.getEmail());
         return userRepository.findAll();
     }
-    void sendEmail() {
+
+    @PostMapping("/authenticate")
+    public String dologin(@RequestBody LoginRequest loginRequest){
+        Authentication authentication = authenticationManager.authenticate(new UsernamePasswordAuthenticationToken(
+                loginRequest.getUsername(), loginRequest.getPassword()
+        ));
+        SecurityContextHolder.getContext().setAuthentication(authentication);
+        return "Login";
+    }
+
+    String sendEmail(String email) {
 
         SimpleMailMessage msg = new SimpleMailMessage();
-        msg.setTo("bhoomildayani05@gmail.com", "to_2@gmail.com", "to_3@yahoo.com");
+        msg.setTo(email);
 
         msg.setSubject("Testing from Spring Boot");
         msg.setText("Hello World \n Spring Boot Email");
 
         javaMailSender.send(msg);
 
+        return null;
     }
 }
--- src/main/java/in/ac/charusat/studentmgmtsystem/utility/JWTUtility.java
+++ src/main/java/in/ac/charusat/studentmgmtsystem/utility/JWTUtility.java
@@ -1,6 +1,38 @@
-package in.ac.charusat.studentmgmtsystem.utility;
-
-import java.io.Serializable;
-
-public class JWTUtility implements Serializable {
-}
+//package in.ac.charusat.studentmgmtsystem.utility;
+//
+//import io.jsonwebtoken.Claims;
+//import io.jsonwebtoken.Jwts;
+//import org.springframework.beans.factory.annotation.Value;
+//
+//import java.io.Serializable;
+//import java.util.Date;
+//import java.util.function.Function;
+//
+//public class JWTUtility implements Serializable {
+//
+//    private static final long serialVersionUID=234234523523L;
+//    private static final long JWT_TOKEN_VELIDITY=5*60*50;
+//
+//    @Value("${jwt.secret}")
+//    private String secretKey;
+//
+//    public String getUsernameFromToken(String token){
+//        return getCliamFromToken(token, Claims::getSubject);
+//    }
+//
+//    public Date getExpirationDateFromToken(String token){
+//        return getCliamFromToken(token, Claims::getExpiration);
+//    }
+//
+//    private <T> T getCliamFromToken(String token, Function<Claims, T> claimsTFunction) {
+//        final Claims claims = getAllCliamFromToken(token);
+//        return claimsResolver.apply(claims);
+//    }
+//
+//    private Claims getAllCliamFromToken(String token) {
+//        return Jwts.parser().setSigningKey(secretKey).parseClaimsJwt(token).getBody();
+//    }
+//
+//
+//
+//}
